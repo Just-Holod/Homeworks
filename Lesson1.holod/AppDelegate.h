f9943c30a1af14c044d7af67feea04b6d16e3111
Серый <macbookair@Seruy-2.local>
2014-04-02 18:50:56 +0300

    Initial Commit

---
+++ Lesson1.holod/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lesson1.holod
+//
+//  Created by Серый on 02.04.14.
+//  Copyright (c) 2014 progschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lesson1.holod/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lesson1.holod
+//
+//  Created by Серый on 02.04.14.
+//  Copyright (c) 2014 progschool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
